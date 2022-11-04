6becd0401617c9c07ed7c058c063c90246abd8cb
enotarangelo <93266827+enotarangelo@users.noreply.github.com>
2022-11-04 17:34:51 +0100

    refactoring and management error case

--- server.js
+++ server.js
@@ -22,21 +22,55 @@ app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({ extended: true })); 
 
 app.post('/', async (req, res) => {
-    let coordinates= JSON.stringify(req.body.coordinates);
-    let address=  JSON.stringify(req.body.address);
-    if ( (typeof coordinates === 'undefined' && address.length === 2)||(typeof address === 'undefined' && coordinates.length === 2)) {
-      res.sendFile(path.join(__dirname+'/public/error.html'))
-   }
-   else{
-      let ris_db, latitudine, longitudine, farm, ind, lat_f, lon_f;
-      
-      if (typeof coordinates !== 'undefined') {
-        if (utils.check_coordinates(coordinates)===false){
+  let coordinates= JSON.stringify(req.body.coordinates);
+  let address=  JSON.stringify(req.body.address);
+  if ( (typeof coordinates === 'undefined' && address.length === 2)||(typeof address === 'undefined' && coordinates.length === 2)) {
+    res.sendFile(path.join(__dirname+'/public/error.html'))
+  }
+  else{
+    let ris_db, latitudine, longitudine, farm, ind, lat_f, lon_f;
+    
+    if (typeof coordinates !== 'undefined') {
+      if (utils.check_coordinates(coordinates)===false){
+        res.sendFile(path.join(__dirname+'/public/error.html'))
+      }else{
+        let ris= utils.string_to_coordinates(coordinates)
+        latitudine = ris[0];
+        longitudine = ris[1];
+        ris_db= await dbFunctions.getCoordinates(longitudine, latitudine)
+        farm= ris_db[0].properties.DENOM_FARMACIA;
+        ind= ris_db[0].properties.INDIRIZZO;
+        tel= ris_db[0].properties.TELEFONO;
+        console.log(farm +' '+ ind + ' '+ tel)
+        lat_f= ris_db[0].properties.LAT;
+        lon_f= ris_db[0].properties.LNG;
+        //distanza a piedi e in macchina
+        var time_dis= time_distance({ lat: latitudine, lng: longitudine },  { lat: lat_f, lng: lon_f });
+        time_feet= time_dis[0];
+        time_car= time_dis[1];
+        //res.send(farm +'Indirizzo: '+ ind + '\n Numero di telefono: '+ tel+ '\n '+ time_feet+ ' minuti a piedi \n'+time_car +' minuti in macchina')
+        res.send(utils.html_result+`
+            <h1>`+farm +`</h1>
+            <p>`+ind+`</p>
+            <p>`+tel+`</p>
+            <p>`+time_feet+` minuti a piedi</p>
+            <p>`+time_car+` minuti in macchina</p><br>
+            `+utils.html_result_end
+        )
+      }
+    }
+    if (typeof address !== 'undefined') {
+      var NodeGeocoder = require('node-geocoder');
+      var geocoder = NodeGeocoder({
+      provider: 'openstreetmap',
+      });
+
+      geocoder.geocode(address, async function(err, ris) { 
+        if (ris.length===0){
           res.sendFile(path.join(__dirname+'/public/error.html'))
         }else{
-          let ris= utils.string_to_coordinates(coordinates)
-          latitudine = ris[0];
-          longitudine = ris[1];
+          latitudine = ris[0].latitude;
+          longitudine = ris[0].longitude;
           ris_db= await dbFunctions.getCoordinates(longitudine, latitudine)
           farm= ris_db[0].properties.DENOM_FARMACIA;
           ind= ris_db[0].properties.INDIRIZZO;
@@ -48,109 +82,18 @@ app.post('/', async (req, res) => {
           var time_dis= time_distance({ lat: latitudine, lng: longitudine },  { lat: lat_f, lng: lon_f });
           time_feet= time_dis[0];
           time_car= time_dis[1];
-          //res.send(farm +'Indirizzo: '+ ind + '\n Numero di telefono: '+ tel+ '\n '+ time_feet+ ' minuti a piedi \n'+time_car +' minuti in macchina')
-          res.send(`
-          <head>
-            <title>Cherry | Farmacy</title>
-            <!-- icon -->
-            <link rel="shortcut icon" href="./public/img/logo.webp" type="image/x-icon" />
-            <meta charset="utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <!-- Bootstrap -->
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-            <!-- main css -->
-            <link rel="stylesheet" href="./public/app.css" />
-          </head>
-          <body>
-            <nav class="nav primary-nav">
-                <ul>
-                    <li><a href="./public/app.html"><img src="./public/img/logo.webp" style="width:50px" alt="Image"></a></li>
-                    <li>Cherry</li>
-                </ul>
-            </nav>
-  
-            <div class="intro">
-              <img src="./public/img/logo_farmacy.png" style="width:15%" alt="Image">
-              <h1>Cherry <span style="color:#FC2438">Farmacy</span></h1>      
-              <p>Ecco la farmacia più vicina: </p><br>
-              <p>___________</p><br>
-              <h1>`+farm +`</h1>
-              <p>`+ind+`</p>
-              <p>`+tel+`</p>
-              <p>`+time_feet+` minuti a piedi</p>
-              <p>`+time_car+` minuti in macchina</p><br>
-              <a href="./public/app.html"><button type="button" style="background-color: #2F4858; border: none;  padding: 15px 32px; border-radius: 100px; color:#FFF; font-size: medium;">Nuova ricerca</button></a><br>
-            </div>
-          </body>`)
-        }
+          res.send(utils.html_result+`
+          <h1>`+farm +`</h1>
+          <p>`+ind+`</p>
+          <p>`+tel+`</p>
+          <p>`+time_feet+` minuti a piedi</p>
+          <p>`+time_car+` minuti in macchina</p><br>
+          `+utils.html_result_end
+          )                       
+        }        
+      });
     }
-      if (typeof address !== 'undefined') {
-        var NodeGeocoder = require('node-geocoder');
-        var geocoder = NodeGeocoder({
-        provider: 'openstreetmap',
-        });
-
-
-        geocoder.geocode(address, async function(err, ris) { 
-              if (ris.length===0){
-                res.sendFile(path.join(__dirname+'/public/error.html'))
-              }else{
-                latitudine = ris[0].latitude;
-                longitudine = ris[0].longitude;
-                ris_db= await dbFunctions.getCoordinates(longitudine, latitudine)
-                farm= ris_db[0].properties.DENOM_FARMACIA;
-                ind= ris_db[0].properties.INDIRIZZO;
-                tel= ris_db[0].properties.TELEFONO;
-                console.log(farm +' '+ ind + ' '+ tel)
-                lat_f= ris_db[0].properties.LAT;
-                lon_f= ris_db[0].properties.LNG;
-                //distanza a piedi e in macchina
-                var time_dis= time_distance({ lat: latitudine, lng: longitudine },  { lat: lat_f, lng: lon_f });
-                time_feet= time_dis[0];
-                time_car= time_dis[1];
-                              
-              res.send(`
-              <head>
-                <title>Cherry | Farmacy</title>
-                <!-- icon -->
-                <link rel="shortcut icon" href="./public/img/logo.webp" type="image/x-icon" />
-                <meta charset="utf-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <!-- Bootstrap -->
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-                <!-- main css -->
-                <link rel="stylesheet" href="./public/app.css" />
-              </head>
-              <body>
-                <nav class="nav primary-nav">
-                    <ul>
-                        <li><a href="./public/app.html"><img src="./public/img/logo.webp" style="width:50px" alt="Image"></a></li>
-                        <li>Cherry</li>
-                    </ul>
-                </nav>
-          
-                <div class="intro">
-                  <img src="./public/img/logo_farmacy.png" style="width:15%" alt="Image">
-                  <h1>Cherry <span style="color:#FC2438">Farmacy</span></h1>      
-                  <p>Ecco la farmacia più vicina: </p><br>
-                  <p>___________</p><br>
-                  <h1 style='color:#'>`+farm +`</h1>
-                  <p>`+ind+`</p>
-                  <p>`+tel+`</p>
-                  <p>`+time_feet+` minuti a piedi</p>
-                  <p>`+time_car+` minuti in macchina</p><br>
-                  <a href="./public/app.html"><button type="button" style="background-color: #2F4858; border: none;  padding: 15px 32px; border-radius: 100px; color:#FFF; font-size: medium;">Nuova ricerca</button></a><br>
-                </div>
-              </body>`)
-              }
-        
-              });
-            }
-          } 
+  } 
 });
 
 // Start the web server and connect to the database
--- utils.js
+++ utils.js
@@ -23,6 +23,38 @@ module.exports = {
          }
       }
       return bool;
-   }
+   },
+
+   html_result : `
+   <head>
+     <title>Cherry | Farmacy</title>
+     <!-- icon -->
+     <link rel="shortcut icon" href="./public/img/logo.webp" type="image/x-icon" />
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!-- Bootstrap -->
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+     <!-- main css -->
+     <link rel="stylesheet" href="./public/app.css" />
+   </head>
+   <body>
+     <nav class="nav primary-nav">
+         <ul>
+             <li><a href="./public/app.html"><img src="./public/img/logo.webp" style="width:50px" alt="Image"></a></li>
+             <li>Cherry</li>
+         </ul>
+     </nav>
+
+     <div class="intro">
+       <img src="./public/img/logo_farmacy.png" style="width:15%" alt="Image">
+       <h1>Cherry <span style="color:#FC2438">Farmacy</span></h1>      
+       <p>Ecco la farmacia più vicina: </p><br>
+       <p>___________</p><br>`,
+   
+   html_result_end:`<a href="./public/app.html"><button type="button" style="background-color: #2F4858; border: none;  padding: 15px 32px; border-radius: 100px; color:#FFF; font-size: medium;">Nuova ricerca</button></a><br>
+   </div>
+ </body>`
 	
 }
